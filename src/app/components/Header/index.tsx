5329deaf0d529367055ba21940ebcd6fd0ca3098
evaabsalyamova <absalyamovaeva@gmail.com>
2024-09-30 22:17:09 +0400

    Add lightning mode for active header links

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,20 +1,46 @@
 import styles from "./styles.module.css";
 import Link from "next/link";
 
-const Header: React.FunctionComponent = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.logo}>LOGO</div>
-      <div className={styles.linksBlock}>
-        <Link href="#balance">
-          <button className={styles.link}>Преимущества</button>
-        </Link>
-        <Link href="#reports">
-          <button className={styles.link}>Как работаем</button>
-        </Link>
-      </div>
-    </div>
-  );
+interface IProps {
+  isBalanceBlockVisible: boolean;
+  isReportsBlockVisible: boolean;
+}
+
+const activeLinkStyles = {
+  color: "#E0570E",
+  textDecoration: "underline",
+  textUnderlineOffset: "3px",
 };
+
+const Header: React.FunctionComponent<IProps> = ({
+  isBalanceBlockVisible,
+  isReportsBlockVisible,
+}) => (
+  <div className={styles.container}>
+    <div className={styles.logo}>LOGO</div>
+    <div className={styles.linksBlock}>
+      <Link href="#balance">
+        <button
+          style={isBalanceBlockVisible ? activeLinkStyles : undefined}
+          className={styles.link}
+        >
+          Преимущества
+        </button>
+      </Link>
+      <Link href="#reports">
+        <button
+          style={
+            isReportsBlockVisible && !isBalanceBlockVisible
+              ? activeLinkStyles
+              : undefined
+          }
+          className={styles.link}
+        >
+          Как работаем
+        </button>
+      </Link>
+    </div>
+  </div>
+);
 
 export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./styles.module.css";
 import HeaderBlock from "./components/HeaderBlock";
 import BalanceBlock from "./components/BalanceBlock";
@@ -5,15 +7,31 @@ import PartnersBlock from "./components/PartnersBlock";
 import ReportsBlock from "./components/ReportsBlock";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { useState } from "react";
+import VisibilitySensor from "react-visibility-sensor";
 
 export default function Home() {
+  const [isBalanceBlockVisible, setIsBalanceBlockVisible] = useState(false);
+  const [isReportsBlockVisible, setIsReportsBlockVisible] = useState(false);
+
   return (
     <div className={styles.mainContainer}>
-      <Header />
+      <Header
+        isBalanceBlockVisible={isBalanceBlockVisible}
+        isReportsBlockVisible={isReportsBlockVisible}
+      />
       <HeaderBlock />
-      <BalanceBlock />
+
+      <VisibilitySensor partialVisibility onChange={setIsBalanceBlockVisible}>
+        <BalanceBlock />
+      </VisibilitySensor>
+
       <PartnersBlock />
-      <ReportsBlock />
+
+      <VisibilitySensor partialVisibility onChange={setIsReportsBlockVisible}>
+        <ReportsBlock />
+      </VisibilitySensor>
+
       <Footer />
     </div>
   );
