e0a2177f3c6e32a4bb2662083726c1d100cc76cf
evaabsalyamova <absalyamovaeva@gmail.com>
2024-10-02 02:02:21 +0400

    Add adaptive design for `PartnersBlock`

--- src/app/components/PartnersBlock/index.tsx
+++ src/app/components/PartnersBlock/index.tsx
@@ -1,30 +1,59 @@
 /* eslint-disable @next/next/no-img-element */
+import { useMediaQuery } from "react-responsive";
 import styles from "./styles.module.css";
 
 const PartnersBlock: React.FunctionComponent = () => {
+  const isTablet = useMediaQuery({ maxWidth: 1439 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.title}>Наши партнёры по монетизации</div>
         <div className={styles.logos}>
-          <img
-            src="https://pics.st/463/63f/d80c5646.png"
-            alt="Next.js logo"
-            width={177}
-            height={54}
-          />
-          <img
-            src="https://pics.st/49e/82c/7210814a.png"
-            alt="Next.js logo"
-            width={102}
-            height={71}
-          />
-          <img
-            src="https://pics.st/f04/514/904e39a2.png"
-            alt="Next.js logo"
-            width={175}
-            height={45}
-          />
+          {isMobile ? (
+            <>
+              <img
+                src="https://pics.st/463/63f/d80c5646.png"
+                alt="Next.js logo"
+                width={139}
+                height={43}
+              />
+              <img
+                src="https://pics.st/f04/514/904e39a2.png"
+                alt="Next.js logo"
+                width={137}
+                height={35}
+              />
+              <img
+                src="https://pics.st/49e/82c/7210814a.png"
+                alt="Next.js logo"
+                width={85}
+                height={59}
+              />
+            </>
+          ) : (
+            <>
+              <img
+                src="https://pics.st/463/63f/d80c5646.png"
+                alt="Next.js logo"
+                width={isTablet ? 121 : 177}
+                height={isTablet ? 38 : 54}
+              />
+              <img
+                src="https://pics.st/49e/82c/7210814a.png"
+                alt="Next.js logo"
+                width={isTablet ? 70 : 102}
+                height={isTablet ? 49 : 71}
+              />
+              <img
+                src="https://pics.st/f04/514/904e39a2.png"
+                alt="Next.js logo"
+                width={isTablet ? 120 : 175}
+                height={isTablet ? 31 : 45}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
